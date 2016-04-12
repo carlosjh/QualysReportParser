32c67f282486a4b50b3c0fe9db18e9ccfd9a30e1
carlosjh <carlosjhdez_1991@hotmail.com>
2016-04-12 20:01:48 +0200

    Added files via upload

--- HostInfo.java
+++ HostInfo.java
@@ -3,12 +3,69 @@ package qualysreportparser;
 public class HostInfo {
 
     private String ip;
+    private int highSeverity;
+    private int midSeverity;
+    private int lowSeverity;
+    private int informationSeverity;
+    
 
     public HostInfo(String ip) {
         this.ip = ip;
+        highSeverity=0;
+        midSeverity=0;
+        lowSeverity=0;
+        informationSeverity=0;
     }
 
-    public String getIp() {
+    public void sumarInformation(){
+    	informationSeverity = this.informationSeverity+1;
+    }
+    
+    public void sumarHigh(){
+    	highSeverity = this.highSeverity+1;
+    }
+    
+    public void sumarMid(){
+    	midSeverity = this.midSeverity+1;
+    }
+    
+    public void sumarLow(){
+    	lowSeverity = this.lowSeverity+1;
+    }
+    
+	public int getHighSeverity() {
+		return highSeverity;
+	}
+
+	public void setHighSeverity(int highSeverity) {
+		this.highSeverity = highSeverity;
+	}
+
+	public int getMidSeverity() {
+		return midSeverity;
+	}
+
+	public void setMidSeverity(int midSeverity) {
+		this.midSeverity = midSeverity;
+	}
+
+	public int getLowSeverity() {
+		return lowSeverity;
+	}
+
+	public void setLowSeverity(int lowSeverity) {
+		this.lowSeverity = lowSeverity;
+	}
+
+	public int getInformationSeverity() {
+		return informationSeverity;
+	}
+
+	public void setInformationSeverity(int informationSeverity) {
+		this.informationSeverity = informationSeverity;
+	}
+
+	public String getIp() {
         return ip;
     }
 
@@ -20,6 +77,10 @@ public class HostInfo {
     public String toString() {
         return "\t\t<HostInfo>\n"
                 + "\t\t\t<HostIP>" + ip + "</HostIP>\n"
+                +"\t\t\t<HighSeverityVulnerabilities>"+highSeverity+"</HighSeverityVulnerabilities>\n"
+                +"\t\t\t<MediumSeverityVulnerabilities>"+midSeverity+"</MediumSeverityVulnerabilities>\n"
+                +"\t\t\t<LowSeverityVulnerabilities>"+lowSeverity+"</LowSeverityVulnerabilities>\n"
+                +"\t\t\t<InformationalVulnerabilities>"+informationSeverity+"</InformationalVulnerabilities>\n"
                 + "\t\t</HostInfo>\n";
     }
 
--- QualysReportParser.java
+++ QualysReportParser.java
@@ -29,8 +29,7 @@ public class QualysReportParser {
 
     @Override
     public String toString() {
-        String salida = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n\n"
-                + "<VulerabilityReport>\n\t"
+        String salida = /*"<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n\n"*/"<VulnerabilityReport>\n\t"
                 + reportSummary.toString()
                 + "\t<HostsSummary>\n";
 
@@ -71,17 +70,16 @@ public class QualysReportParser {
         boolean isTHREAT = false;
         boolean isSOLUTION = false;
         boolean isCVE = false;
-        boolean isCVEURL = false;
+        boolean isCVEID = false;
 
         /* Name of the file. */
-        if (args.length != 1) {
+        /*if (args.length != 1) {
             throw new RuntimeException("The name of the XML file is required!");
-        }
-        //String text = null;
+        }*/
         try {
             XMLInputFactory factory = XMLInputFactory.newInstance();
-            XMLEventReader eventReader = factory.createXMLEventReader(new FileReader(new File(args[0])));
-            //XMLEventReader eventReader = factory.createXMLEventReader(new FileReader(new File("testHeader.xml")));
+            //XMLEventReader eventReader = factory.createXMLEventReader(new FileReader(new File(args[0])));
+            XMLEventReader eventReader = factory.createXMLEventReader(new FileReader(new File("Scan_Report_Vulnerabilidades_CPD_TELECITY_Irlanda_ndtex.xml")));
             String reportTime = "";
             String totalNumberOfVulerabilities = "";
             String ip = "";
@@ -95,11 +93,9 @@ public class QualysReportParser {
             String impact = "";
             String threat = "";
             String solution = "";
-            String cveurl = "";
-
+            String cveid = "";
             while (eventReader.hasNext()) {
                 XMLEvent event = eventReader.nextEvent();
-
                 switch (event.getEventType()) {
 
                     /* NEW ELEMENT */
@@ -119,7 +115,6 @@ public class QualysReportParser {
                         } else if (qName.equalsIgnoreCase("IP")) {
                             isIP = true;
                         } else if (qName.equalsIgnoreCase("QID")) {
-                            //Separamos QID y QID1 para comprobar cuando pasamos al glosario de las vulnerabilidades
                             if (!glossary) {
                                 isQID = true;
                             } else {
@@ -150,8 +145,8 @@ public class QualysReportParser {
                             isSOLUTION = true;
                         } else if (qName.equalsIgnoreCase("CVE_ID")) {
                             isCVE = true;
-                        } else if (qName.equalsIgnoreCase("URL") && isCVE) {
-                            isCVEURL = true;
+                        } else if (qName.equalsIgnoreCase("ID") && isCVE) {
+                            isCVEID = true;
                         }
                         break;
 
@@ -203,9 +198,9 @@ public class QualysReportParser {
                         } else if (isSOLUTION) {
                             solution = characters.getData();
                             isSOLUTION = false;
-                        } else if (isCVEURL) {
-                            cveurl = characters.getData();
-                            isCVEURL = false;
+                        } else if (isCVEID) {
+                            cveid = characters.getData();
+                            isCVEID = false;
                             isCVE = false;
                         }
                         break;
@@ -223,18 +218,17 @@ public class QualysReportParser {
                                 qrp.hostSummary.add(hostInfo);
                             }
                         } else if (endEl.equalsIgnoreCase("VULN_INFO")) {
-                            SingleVulnerability sv = new SingleVulnerability(qid);
+                            SingleVulnerability sv = new SingleVulnerability(qid,port);
                             sv.getHostIP().add(ip);
-                            sv.setPortNumber(port);
                             sv.setServiceName(service);
-                            //REVISAR EL BID
-                            sv.setBID(qid);//NO VA AQUI EL BID
+                            sv.setBID(qid);
+                            sv.setPortProtocol(protocol);
                             if (qrp.hostVulnerabilities.isEmpty()) {
                                 qrp.hostVulnerabilities.add(sv);
                             } else {
                                 boolean flag = false;
                                 for (int i = 0; i < qrp.hostVulnerabilities.size(); i++) {
-                                    if (qrp.hostVulnerabilities.get(i).getQID().equals(qid)) {
+                                    if (qrp.hostVulnerabilities.get(i).getQID().equals(qid) && qrp.hostVulnerabilities.get(i).getPortNumber().equals(port)) {
                                         qrp.hostVulnerabilities.get(i).getHostIP().add(ip);
                                         flag = true;
                                         break;
@@ -245,6 +239,8 @@ public class QualysReportParser {
                                 }
                             }
                         } else if (endEl.equalsIgnoreCase("VULN_DETAILS")) {
+                        	boolean encontrado= false;
+                        	boolean encontrado1=false;
                             for (int i = 0; i < qrp.hostVulnerabilities.size(); i++) {
                                 if (qrp.hostVulnerabilities.get(i).getQID().equals(qid1)) {
                                     qrp.hostVulnerabilities.get(i).setOriginalDescription(title);
@@ -252,11 +248,68 @@ public class QualysReportParser {
                                     qrp.hostVulnerabilities.get(i).setOtherRef(impact);
                                     qrp.hostVulnerabilities.get(i).setVulnerabilityDescription(threat);
                                     qrp.hostVulnerabilities.get(i).setSolution(solution);
-                                    qrp.hostVulnerabilities.get(i).setCVE(cveurl);
-                                    //MIRAR PROTOCOLO, no va en portProtocol
-                                    qrp.hostVulnerabilities.get(i).setPortProtocol(protocol);//NO va aqui el protocolo
+                                    qrp.hostVulnerabilities.get(i).setCVE(cveid);
+                                    
+                                    for(int j=0; j< qrp.hostVulnerabilities.get(i).getHostIP().size();j++){
+                                    	String hostIP = qrp.hostVulnerabilities.get(i).getHostIP().get(j);
+                                    	for(int z = 0; z<qrp.hostSummary.size();z++){
+                                    		if(qrp.hostSummary.get(z).equals(hostIP)){
+                                    			if(severity.equals("1") || severity.equals("2")){
+                                    				qrp.hostSummary.get(z).setInformationSeverity(qrp.hostSummary.get(z).getInformationSeverity()+1);
+                                    			}else if(severity.equals("3")){
+                                    				qrp.hostSummary.get(z).setLowSeverity(qrp.hostSummary.get(z).getLowSeverity()+1);
+                                    			}else if(severity.equals("4")){
+                                    				qrp.hostSummary.get(z).setMidSeverity(qrp.hostSummary.get(z).getMidSeverity()+1);
+                                    			}else{
+                                    				qrp.hostSummary.get(z).setHighSeverity(qrp.hostSummary.get(z).getHighSeverity()+1);
+                                    			}
+                                    			encontrado1= true;
+                                    			break;
+                                    		}
+                                    	}
+                                    	if(encontrado1){
+                                			break;
+                                		}
+                                    }
+                                    encontrado = true;
+                                    break;
                                 }
                             }
+                            if(!encontrado){
+                            	System.out.println("Nueva vulnerabilidad con severidad: "+severity);
+                            	SingleVulnerability sv = new SingleVulnerability(qid1,port);
+                                sv.setServiceName(service);
+                                sv.setBID(qid);
+                                sv.setPortProtocol(protocol);
+                                sv.setOriginalDescription(title);
+                                sv.setSeverity(severity);
+                                sv.setOtherRef(impact);
+                                sv.setVulnerabilityDescription(threat);
+                                sv.setSolution(solution);
+                                sv.setCVE(cveid);
+                                qrp.hostVulnerabilities.add(sv);
+                            }
+                        } else if (endEl.equalsIgnoreCase("TEMPLATE_DETAILS")) {
+                        	int information = 0;
+                        	int low = 0;
+                        	int mid = 0;
+                        	int high = 0;
+                        	for (int  i = 0; i< qrp.hostVulnerabilities.size(); i++){
+                        		if(qrp.hostVulnerabilities.get(i).getSeverity().equals("1") || qrp.hostVulnerabilities.get(i).getSeverity().equals("2")){
+                        			information ++;
+                        		}else if (qrp.hostVulnerabilities.get(i).getSeverity().equals("3")){
+                        			low++;
+                        		}else if (qrp.hostVulnerabilities.get(i).getSeverity().equals("4")){
+                        			mid++;
+                        		}else{
+                        			high++;
+                        		}
+                        	}
+                        	qrp.reportSummary.getSvs().setHighSeverityVulnerabilities(String.valueOf(high));
+                        	qrp.reportSummary.getSvs().setMediumSeverityVulnerabilities(String.valueOf(mid));
+                        	qrp.reportSummary.getSvs().setLowSeverityVulnerabilities(String.valueOf(low));
+                        	qrp.reportSummary.getSvs().setInformationalVulnerabilities(String.valueOf(information));
+                        	qrp.reportSummary.getSvs().setTotalNumberOfVulnerabilities(String.valueOf(qrp.hostVulnerabilities.size()));
                         }
                         break;
                 }
